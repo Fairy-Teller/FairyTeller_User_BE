07073c097e26344b7f8285295aeaa94876cb2311
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-07 17:08:47 +0900

    소셜로그인(구글) 구현 완료

--- src/main/java/jungle/fairyTeller/security/GoogleUserInfo.java
+++ src/main/java/jungle/fairyTeller/security/GoogleUserInfo.java
@@ -1,4 +1,22 @@
 package jungle.fairyTeller.security;
 
-public class GoogleUserInfo {
+import java.util.Map;
+
+public class GoogleUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getProvider() {
+        return "Google";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
 }
