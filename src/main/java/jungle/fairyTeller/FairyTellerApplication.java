92646226c7ffd29100b9f92ca27caf4f35136851
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-07 14:54:12 +0900

    initial commit

---
+++ src/main/java/jungle/fairyTeller/FairyTellerApplication.java
@@ -0,0 +1,13 @@
+package jungle.fairyTeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairyTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairyTellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jungle/fairyTeller/FairyTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.fairyTeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyTellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
