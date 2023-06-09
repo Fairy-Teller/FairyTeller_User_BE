8d6e594c26008b9737c5c83389c9c139de414854
seo2216 <kkiy2216@gmail.com>
2023-06-09 22:16:02 +0900

    [Add]시나리오 한 줄로 요약하는 api구현

--- src/main/java/jungle/fairyTeller/fairyTale/story/service/ChatGptService.java
+++ src/main/java/jungle/fairyTeller/fairyTale/story/service/ChatGptService.java
@@ -4,6 +4,7 @@ import jungle.fairyTeller.fairyTale.story.dto.ChatGptRequestDto;
 import jungle.fairyTeller.fairyTale.story.dto.ChatGptResponseDto;
 import jungle.fairyTeller.fairyTale.story.dto.QuestionRequestDto;
 import jungle.fairyTeller.config.ChatGptConfig;
+import jungle.fairyTeller.fairyTale.story.dto.SummarizingRequestDto;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -32,8 +33,8 @@ public class ChatGptService {
     }
 
     public ChatGptResponseDto askQuestion(QuestionRequestDto requestDto) {
-        String question = requestPasing(requestDto);
-        System.out.println(question);
+        String question = requestParsing(requestDto);
+        System.out.println("시나리오 작성:"+question);
         return this.getResponse(
                 this.buildHttpEntity(
                         new ChatGptRequestDto(
@@ -47,13 +48,45 @@ public class ChatGptService {
         );
     }
 
-    public String requestPasing(QuestionRequestDto requestDto){
-        return "'"+requestDto.getParameter1()+"',"
-                +"'"+requestDto.getParameter2()+"',"
-                +"'"+requestDto.getParameter3()+"'"
-                +"를 가지고 1문단에 100자이내, 총 3문단짜리 2~5세를 위한 동화를 한글로 만들어줘";
-//        return "Please make a fairy tale for 2-5 year olds with '"
-//        +requestDto.getParameter1()+"',"+"'"+requestDto.getParameter2()+"',"
-//        +"and '"+requestDto.getParameter3()+"'"+ "with less than 70 characters per paragraph";
+    public ChatGptResponseDto askSummarize(SummarizingRequestDto requestDto){
+        String question = requestParsingToSummarize(requestDto);
+        System.out.println("한 줄 요약:"+question);
+        return this.getResponse(
+                this.buildHttpEntity(
+                        new ChatGptRequestDto(
+                                ChatGptConfig.MODEL,
+                                question,
+                                ChatGptConfig.MAX_TOKEN,
+                                ChatGptConfig.TEMPERATURE,
+                                ChatGptConfig.TOP_P
+                        )
+                )
+        );
+    }
+
+    public String requestParsing(QuestionRequestDto requestDto){
+//        return "'"+requestDto.getParameter1()+"',"
+//                +"'"+requestDto.getParameter2()+"',"
+//                +"'"+requestDto.getParameter3()+"'"
+//                +"를 가지고 1문단에 100자이내, 총 3문단짜리 2~5세를 위한 동화를 영어로 만들어줘";
+        return "Please make a fairy tale for 2-5 year olds with '"
+        +requestDto.getParameter1()+"',"+"'"+requestDto.getParameter2()+"',"
+        +"and '"+requestDto.getParameter3()+"'"+ "with less than 150 characters per paragraph";
+    }
+
+    public String requestParsingToSummarize(SummarizingRequestDto requestDto){
+        String tmpText = requestDto.getText();
+        String targetParagraph = "\n\n";
+        int startIndex = tmpText.indexOf(targetParagraph);
+        String parsedParagraph = "";
+        if (startIndex != -1) {
+            startIndex += targetParagraph.length();
+            int endIndex = tmpText.indexOf("\n\n", startIndex);
+            if (endIndex != -1) {
+                parsedParagraph += tmpText.substring(startIndex, endIndex);
+            }
+        }
+        //return "Summarize a line from"+"'"+parsedParagraph+"'";
+        return "'"+parsedParagraph+"'"+"Please summarize a line in English";
     }
 }
