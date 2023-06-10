1780740661cc3322d9f9f46ab092fd2477f6d3f6
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-10 19:25:25 +0900

    [Refactor] dev server 테스트 가능하도록 application.properties 수정

--- src/main/java/jungle/fairyTeller/fairyTale/story/controller/KeywordController.java
+++ src/main/java/jungle/fairyTeller/fairyTale/story/controller/KeywordController.java
@@ -1,13 +1,14 @@
 package jungle.fairyTeller.fairyTale.story.controller;
 
 import jungle.fairyTeller.fairyTale.story.entity.KeywordEnum;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KeywordController {
     @GetMapping("/fairytale")
-    public KeywordEnum[] getKeywords(){
+    public KeywordEnum[] getKeywords(@AuthenticationPrincipal String userId){
         return KeywordEnum.values();
     }
 }
