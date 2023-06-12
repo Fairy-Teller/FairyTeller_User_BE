8a84aafb1925acdd298ea435996e5086a91dc383
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-12 22:15:06 +0900

    [Refactor] CICD Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,18 @@
 REPOSITORY=/home/ubuntu/be
 cd $REPOSITORY
 
-APP_NAME=moduform
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+APP_LOG = "$REPOSITORY/application.log"
+ERROR_LOG = "$REPOSITORY/error.log"
+DEPLOY_LOG = "$REPOSITORY/deploy.log"
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할 애플리케이션이 없습니다."
-else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+TIME_NOW = $(date + %c)
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null
+echo "$TIME_NOW> Deploy - $JAR_PATH "
+echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_PATH > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다" >> $DEPLOY_LOG
