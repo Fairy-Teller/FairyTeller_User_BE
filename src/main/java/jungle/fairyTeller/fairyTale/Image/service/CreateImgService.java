3653dce831c0728cc01866f16d445f8b26d7a44c
bagjihui <jihi32123@gmail.com>
2023-06-12 18:16:03 +0900

    [Feature] 이미지 생성 함수 추가(이미지 가로,세로, 크기 설정)

--- src/main/java/jungle/fairyTeller/fairyTale/Image/service/CreateImgService.java
+++ src/main/java/jungle/fairyTeller/fairyTale/Image/service/CreateImgService.java
@@ -38,7 +38,28 @@ public class CreateImgService {
         CreateImgResponseDTO responseBody = response.getBody();
 
         return responseBody.getImages().get(0);
+    }
+    public String createImgDetail(String prompt, int height, int width){
+        RestTemplate restTemplate = new RestTemplate();
 
+        // POST 요청에 필요한 데이터를 객체에 담기
+        CreateImgRequestDTO requestObject = new CreateImgRequestDTO(1,7,0,false,0,0,0,height,1,"",prompt,false,0,1,0,0,"Euler a",-1,-1,-1,20,new ArrayList(),-1,0,false, width);
+
+        // 요청 헤더 설정 (선택적)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // 헤더에 필요한 정보 추가
+//         headers.set("HeaderName", "HeaderValue");
+        // 요청 객체 생성
+        HttpEntity<CreateImgRequestDTO> requestEntity = new HttpEntity<>(requestObject, headers);
+
+        // POST 요청 보내기
+        ResponseEntity<CreateImgResponseDTO> response = restTemplate.postForEntity(MlServerUrl, requestEntity, CreateImgResponseDTO.class);
+
+        // 응답 받기
+        CreateImgResponseDTO responseBody = response.getBody();
+
+        return responseBody.getImages().get(0);
     }
 
 }
