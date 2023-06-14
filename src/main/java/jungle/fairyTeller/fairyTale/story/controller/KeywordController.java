a750560e6fcf84636951d7706d20f9361fb72640
seo2216 <kkiy2216@gmail.com>
2023-06-14 16:12:11 +0900

    [Fix]keyword변경

--- src/main/java/jungle/fairyTeller/fairyTale/story/controller/KeywordController.java
+++ src/main/java/jungle/fairyTeller/fairyTale/story/controller/KeywordController.java
@@ -19,15 +19,15 @@ public class KeywordController {
         options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "강아지"));
         options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "고양이"));
         options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "공룡"));
-        options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "개구리"));
-        options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "여우"));
+        options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "토끼"));
+        options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "돼지"));
         options.add(new KeywordOptionDto(KeywordEnum.ANIMAL, "판다"));
         options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "엄마"));
         options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "아빠"));
-        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "경찰관"));
-        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "의사"));
-        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "요리사"));
-        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "선생님"));
+        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "구름"));
+        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "사탕"));
+        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "공주님"));
+        options.add(new KeywordOptionDto(KeywordEnum.PEOPLE, "왕자님"));
 
         return ResponseEntity.ok().body(options);
     }
