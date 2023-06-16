a73738654232d7579a3d2a2890ccd3db10e00eba
seo2216 <kkiy2216@gmail.com>
2023-06-17 04:50:14 +0900

    [Add]문장 번역 추가

--- src/main/java/jungle/fairyTeller/fairyTale/story/dto/SummarizingRequestDto.java
+++ src/main/java/jungle/fairyTeller/fairyTale/story/dto/SummarizingRequestDto.java
@@ -1,9 +1,20 @@
 package jungle.fairyTeller.fairyTale.story.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
+
+@NoArgsConstructor
 @Getter
-public class SummarizingRequestDto implements Serializable {
-    private String text;
+@Setter
+public class ResultDTO implements Serializable {
+    private String translatedText;
+
+    @Builder
+    public ResultDTO(String translatedText) {
+        this.translatedText = translatedText;
+    }
 }
