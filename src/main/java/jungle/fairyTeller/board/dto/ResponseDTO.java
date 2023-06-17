e6e3f429d4e352a91c94ae4860df77a63d8899d9
flyduck-dev <flyduck0dev@gmail.com>
2023-06-17 15:07:21 +0900

    [Refactor] ResponseDTO 추가

--- src/main/java/jungle/fairyTeller/board/dto/ResponseDTO.java
+++ src/main/java/jungle/fairyTeller/board/dto/ResponseDTO.java
@@ -1,2 +1,17 @@
-package jungle.fairyTeller.board.dto;public class ResponseDTO {
+package jungle.fairyTeller.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO<T> {
+    private String error;
+    private List<T> data;
 }
