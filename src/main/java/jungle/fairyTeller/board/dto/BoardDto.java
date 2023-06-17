ecca7b74679c2ea6e583392636ea5344b85a5e0f
flyduck-dev <flyduck0dev@gmail.com>
2023-06-17 20:57:34 +0900

    [Refactor] BoardDto 수정

--- src/main/java/jungle/fairyTeller/board/dto/BoardDto.java
+++ src/main/java/jungle/fairyTeller/board/dto/BoardDto.java
@@ -1,12 +1,15 @@
 package jungle.fairyTeller.board.dto;
 
 import jungle.fairyTeller.board.entity.BoardEntity;
+import jungle.fairyTeller.fairyTale.book.dto.PageDTO;
+import jungle.fairyTeller.board.entity.CommentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -16,34 +19,14 @@ import java.util.List;
 public class BoardDto {
     private Integer boardId;
     private Integer bookId;
-    private Integer author;
-    private String nickname;
     private String title;
-    private String content;
+    private String description;
     private String thumbnailUrl;
+    private Date createdDatetime;
+    private Integer authorId;
+    private String nickname;
+    private List<PageDTO> pages;
     private List<CommentDto> comments;
-    private boolean editable; // New property for indicating editability
 
-    public BoardDto(final BoardEntity entity) {
-        this.boardId = entity.getBoardId();
-        this.bookId = entity.getBookId();
-        this.author = entity.getAuthor();
-        this.nickname = entity.getNickname();
-        this.title = entity.getTitle();
-        this.thumbnailUrl = entity.getThumbnailUrl();
-        this.content = entity.getContent();
-        this.comments = new ArrayList<>();
-    }
-
-    public static BoardEntity toEntity(final BoardDto dto) {
-        return BoardEntity.builder()
-                .boardId(dto.getBoardId())
-                .bookId(dto.getBookId())
-                .author(dto.getAuthor())
-                .nickname(dto.getNickname())
-                .title(dto.getTitle())
-                .content(dto.getContent())
-                .thumbnailUrl(dto.getThumbnailUrl())
-                .build();
-    }
+    //private boolean editable;
 }
