7bf398f061005671c61cd31a361582cfea57b04f
seo2216 <kkiy2216@gmail.com>
2023-06-17 19:50:13 +0900

    [Refactor]키워드 영어로 변환

--- src/main/java/jungle/fairyTeller/fairyTale/story/dto/chatGpt/QuestionRequestDto.java
+++ src/main/java/jungle/fairyTeller/fairyTale/story/dto/chatGpt/QuestionRequestDto.java
@@ -1,10 +1,11 @@
 package jungle.fairyTeller.fairyTale.story.dto.chatGpt;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
-
+@Setter
 @Getter
 public class QuestionRequestDto implements Serializable {
     private String parameter1;
