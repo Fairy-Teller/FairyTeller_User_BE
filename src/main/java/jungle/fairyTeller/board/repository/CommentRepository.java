9bea8ba92b8fe569c4d26d8b3002aa0c9e52ea72
flyduck-dev <flyduck0dev@gmail.com>
2023-06-17 15:36:34 +0900

    [Refactor] Repository 정리

--- src/main/java/jungle/fairyTeller/board/repository/CommentRepository.java
+++ src/main/java/jungle/fairyTeller/board/repository/CommentRepository.java
@@ -1,12 +1,10 @@
 package jungle.fairyTeller.board.repository;
 
 import jungle.fairyTeller.board.entity.CommentEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
-    Page<CommentEntity> findAllByBoardId(Integer boardId, Pageable pageable);
+    List<CommentEntity> findByBoardId(Integer boardId);
 }
